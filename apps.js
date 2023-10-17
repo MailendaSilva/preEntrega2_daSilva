453779f4246bca2a225242c8b7e29538533560fa
Mailen da Silva <mailenfreitas@gmail.com>
2023-10-17 10:23:03 -0300

    subir repo

---
+++ apps.js
@@ -0,0 +1,61 @@
+let carrito = [];
+
+//funcion mostrar pedido
+function pedidoListo() {
+    alert("Tu pedido está listo! Avisale al cliente que en en lapso de veinte minutos será entregado!")
+}
+//array con listado de productos. Esto estará en cards con boostrap y el boton sera agregar al carrito
+const menu = 
+[
+    {combo: "1", precio: 2500, disponibilidad:true, descripcion: "hamburguesa simple con papas y gaseosa"},
+    {combo: "2", precio: 2900, disponibilidad:true, descripcion: "hamburguesa doble con papas y gaseosa"},
+    {combo: "3", precio: 3500, disponibilidad:true, descripcion: "hamburguesa triple con papas y gaseosa"},
+    {combo: "4", precio: 2400, disponibilidad:true, descripcion: "nuggets con papas y gaseosa"},
+    {combo: "5", precio: 1900, disponibilidad:true, descripcion: "menu infantil"}
+]
+console.log(menu)
+//indicar mesa
+const mesa = prompt ("Introduce numero de mesa")
+
+//agregar productos
+while (true) {
+  const nombre = prompt("Introduce el pedido del cliente o FINALIZAR");
+  if (nombre === 'FINALIZAR') {
+    break;
+  }
+  const precio = parseFloat(prompt("Introduce el precio"));
+  if (!isNaN(precio)) {
+    const producto = {
+      nombre: nombre,
+      precio: precio
+    };
+    
+    carrito.push(producto);
+    alert(`${nombre} se agregó al carrito.`);
+  } else {
+    alert('Precio incorrecto. No se agregó');
+  }
+}
+
+// Mostrar la compra x consola
+function mostrarCompra() 
+{
+    console.log("Productos comprados:");
+    carrito.forEach((producto, index) => {
+      console.log(`Nombre: ${producto.nombre}, Precio: $${producto.precio}, Mesa: ${mesa}`);
+    });
+}
+mostrarCompra();
+
+// Funcion SUMA
+function calcularTotal()
+{
+    const total = carrito.reduce((acumulador, producto) => acumulador + producto.precio, 0);
+    console.log(`Cobrar al cliente: $${total}`);
+}
+calcularTotal();
+
+//mostrar pedido
+pedidoListo();
+
+
